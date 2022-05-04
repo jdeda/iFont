3ce5ea23426685a286cde5f9e6acc61345771ade
jdeda <jldeda00@gmail.com>
2022-05-04 15:39:08 -0400

    Initial Commit

---
+++ iFont/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iFont
+//
+//  Created by Jesse Deda on 5/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iFont/iFontApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFontApp.swift
+//  iFont
+//
+//  Created by Jesse Deda on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct iFontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
