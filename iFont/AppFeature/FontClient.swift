1c91293c29cc750352197dcca261f3415d379a84
jdeda <jldeda00@gmail.com>
2022-06-13 08:54:24 -0400

    cleaned

--- iFont/AppFeature/FontClient.swift
+++ iFont/AppFeature/FontClient.swift
@@ -5,11 +5,10 @@
 //  Created by Jesse Deda on 6/2/22.
 //
 
-import Foundation
-import UniformTypeIdentifiers
 import ComposableArchitecture
-import Cocoa
 import Combine
+import Cocoa
+import UniformTypeIdentifiers
 
 struct FontClient {
   struct FetchFontsID: Hashable {}
@@ -19,98 +18,71 @@ struct FontClient {
 extension FontClient {
   public static let live = Self.init(
     fetchFonts: { directory in
-      let foo = directory
-        .fontFilePublisherV2
-        // .filter(isFont)
-        .map { url -> URL in
-          let fileType = url.contentType?.identifier ?? "unknown"
-          
-          // NSLog("path: \(url.path)")
-          NSLog("contentType: \(fileType)")
-          return url
-        }
+      let effect = directory
+        .lazyFontFilePublisher
+        .filter(FontClientHelper.isFont)
         .compactMap(FontClientHelper.makeFonts)
         .map { fonts -> [Font] in
           NSLog("font: \(fonts.count)")
-          return fonts.map { nsFont in
-            Font(fontName: nsFont.fontName)
-          }
+          return fonts.map { Font(fontName: $0.fontName) }
         }
         .eraseToAnyPublisher()
         .eraseToEffect()
-      
-      return foo
+      return effect
     }
   )
 }
 
 struct FontClientHelper {
-  func isFont(_ url: URL) -> Bool {
+  static func isFont(_ url: URL) -> Bool {
     let fileType = url.contentType?.identifier ?? "unknown"
-    
-    // NSLog("path: \(url.path)")
-    NSLog("contentType: \(fileType)")
+    NSLog("contentType: of \(url.path): \(fileType)")
     return fileType.contains("font")
   }
   
   static func makeFonts(_ url: URL) -> [NSFont] {
-    // NSFont(name: $0, size: 25.0)
     guard let array = CTFontManagerCreateFontDescriptorsFromURL(url as CFURL)
     else { return [] }
     guard let fontDescriptors = array as? [CTFontDescriptor]
     else { return [] }
-    // return CTFontCreateWithFontDescriptor(fd[0], 20.0, nil)
-    
-    let foo = fontDescriptors.compactMap { fontDescriptor in
-      NSFont.init(descriptor: fontDescriptor, size: 12)
-    }
-    NSLog("found: \(foo.count) fonts for: \(url.path)")
-    return foo
+    let fonts = fontDescriptors.compactMap { NSFont.init(descriptor: $0, size: 12) }
+    NSLog("found: \(fonts.count) fonts for: \(url.path)")
+    return fonts
   }
 }
 
 extension URL {
   public var contentType: UTType? {
-    let foo = (try? self.resourceValues(forKeys: [.contentTypeKey]))?
-      .contentType
-    return foo
+    (try? self.resourceValues(forKeys: [.contentTypeKey]))?.contentType
   }
   
-  // This is an overly eager publisher,
-  // ie: it could start work even we before we .sink
+  // This is an eager publisher, i.e. could start working before .sink.
   private var eagerFontFilePublisher: PassthroughSubject<URL, Never> {
     let passthrough: PassthroughSubject<URL, Never> = PassthroughSubject()
     
+    // Start work later, but it could happen too soon.
     DispatchQueue.global().async {
-      // start the work later, but it could happen not later enough
-      NSLog("Thread: \(Thread.current)")
-      NSLog("Starting ...")
+      NSLog("\n\nThread: \(Thread.current) \n Starting ...")
       
-      let manager = FileManager.default
-      let keys: [URLResourceKey] = [.fileResourceTypeKey, .contentTypeKey, .nameKey]
-      let options = FileManager.DirectoryEnumerationOptions.skipsHiddenFiles
-      let handler: (URL, Error) -> Bool = { url, error in
-        print("DirectoryEnumerator error at \(url): ", error)
-        return true
-      }
-      let enumerator = manager.enumerator(
+      NSLog("Creating enumerator ...")
+      let enumerator = FileManager.default.enumerator(
         at: self,
-        includingPropertiesForKeys: keys,
-        options: options,
-        errorHandler: handler
+        includingPropertiesForKeys: [.fileResourceTypeKey, .contentTypeKey, .nameKey],
+        options: .skipsHiddenFiles,
+        errorHandler: {
+          print("DirectoryEnumerator error at \($0): ", $1)
+          return true
+        }
       )!
+      NSLog("Finished Creating enumerator ...")
       
-      NSLog("Iterating ...")
-      enumerator.forEach { something in
-        guard let url = something as? URL
-        else { return }
+      
+      NSLog("Enumerating ...")
+      enumerator.forEach { element in
+        guard let url = element as? URL else { return }
         passthrough.send(url)
       }
-      //      for case let url as URL  in enumerator {
-      //        passthrough.send(url)
-      //        // print(url)
-      //      }
-      NSLog("Completed ...")
+      NSLog("Completed Enumeration ...\n\n")
       passthrough.send(completion: .finished)
     }
     
@@ -121,16 +93,14 @@ extension URL {
     eagerFontFilePublisher.eraseToAnyPublisher()
   }
   
-  // this will start the work when the downstream calls .sink
-  var fontFilePublisherV2: AnyPublisher<URL, Never> {
-    let foo = Deferred { () -> PassthroughSubject<URL, Never> in
+  // This will start the work when downstream calls .sink.
+  var lazyFontFilePublisher: AnyPublisher<URL, Never> {
+    Deferred { () -> PassthroughSubject<URL, Never> in
       NSLog("Thread: \(Thread.current)")
       return eagerFontFilePublisher
     }
-      .subscribe(on: DispatchQueue.global())
-      .eraseToAnyPublisher()
-    
-    return foo
+    .subscribe(on: DispatchQueue.global())
+    .eraseToAnyPublisher()
   }
 }
 
