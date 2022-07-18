59961333616eba79da8af63921f5bafd47a40d9b
jdeda <jldeda00@gmail.com>
2022-07-18 15:50:54 -0400

    from previous commit

--- iFont/AppFeature/ItemTypePreviewFeature/ItemPreviewType.swift
+++ iFont/AppFeature/ItemTypePreviewFeature/ItemPreviewType.swift
@@ -13,3 +13,21 @@ enum ItemPreviewType: CaseIterable {
     case custom
     case info
 }
+
+extension ItemPreviewType {
+    var image: Image {
+        switch self {
+        case .sample:
+            return Image(systemName: "text.aligncenter")
+
+        case .repertoire:
+            return Image(systemName: "square.grid.2x2")
+            
+        case .custom:
+            return Image(systemName: "character.cursor.ibeam")
+            
+        case .info:
+            return Image(systemName: "info.circle")
+        }
+    }
+}
