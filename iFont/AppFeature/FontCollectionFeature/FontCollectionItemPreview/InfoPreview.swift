77984629d9c4db10d759b2c2cecdc841e44b0ffd
jdeda <jldeda00@gmail.com>
2022-07-26 16:35:54 -0400

    cleaned up InfoPreview

--- iFont/AppFeature/FontCollectionFeature/FontCollectionItemPreview/InfoPreview.swift
+++ iFont/AppFeature/FontCollectionFeature/FontCollectionItemPreview/InfoPreview.swift
@@ -1,12 +1,79 @@
 //
-//  FontInfoPreview.swift
+//  ItemTypeInfoPreview.swift
 //  iFont
 //
-//  Created by Jesse Deda on 7/18/22.
+//  Created by Jesse Deda on 7/5/22.
 //
 
 import SwiftUI
 import ComposableArchitecture
+
+struct InfoPreview: View {
+    let fonts: [Font]
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(fonts) { font in
+                    FontInfoPreview(font: font)
+                }
+            }
+        }
+    }
+}
+
+struct ItemTypeInfoPreview_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoPreview(fonts: mock_fonts)
+    }
+}
+
+//struct InfoPreview: View {
+//    var item: FontCollectionItem
+//
+//    var body: some View {
+//        ScrollView {
+//            switch item {
+//            case let .font(font):
+//                FontInfoPreview(font: font)
+//            case let .fontFamily(fontFamily):
+//                FontFamilyInfoPreview(family: fontFamily)
+//            }
+//        }
+//    }
+//}
+//
+//struct ItemTypeInfoPreview_Previews: PreviewProvider {
+//    static var previews: some View {
+//
+//        let attributes: [FontAttributeKey: String] =  {
+//            var ats = Dictionary(
+//                uniqueKeysWithValues: zip(
+//                    FontAttributeKey.allCases,
+//                    String.alphabet.accumulatingStrings()
+//                )
+//            )
+//            ats[.copyright] = String.alphabet + String.alphabet + String.alphabet + String.alphabet
+//            return ats
+//        }()
+//
+//        let fonts = (1...10).map { int in
+//            Font(
+//                url: URL(fileURLWithPath: NSTemporaryDirectory()),
+//                name: "Chicken \(int)",
+//                familyName: "Cheese",
+//                attributes: attributes
+//            )
+//        }
+//
+//        InfoPreview(
+//            item: FontCollectionItem.fontFamily(FontFamily(
+//                name: "Cheese",
+//                fonts: fonts)
+//            )
+//        )
+//    }
+//}
 
 struct FontInfoPreview: View {
     var font: Font
