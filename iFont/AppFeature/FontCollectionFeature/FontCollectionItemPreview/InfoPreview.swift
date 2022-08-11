d011fc0da22228e669174abad8dec7d7247a4d8a
jdeda <jldeda00@gmail.com>
2022-08-11 15:41:33 -0400

    added padding removed old code

--- iFont/AppFeature/FontCollectionFeature/FontCollectionItemPreview/InfoPreview.swift
+++ iFont/AppFeature/FontCollectionFeature/FontCollectionItemPreview/InfoPreview.swift
@@ -16,6 +16,7 @@ struct InfoPreview: View {
             LazyVStack {
                 ForEach(fonts) { font in
                     FontInfoPreview(font: font)
+                        .padding(20)
                 }
             }
         }
@@ -27,55 +28,6 @@ struct ItemTypeInfoPreview_Previews: PreviewProvider {
         InfoPreview(fonts: mock_fonts)
     }
 }
-
-
-// MARK: This is what we had before changing the switch statement to just a list. This is more convoluted.
-//struct InfoPreview: View {
-//    var item: FontCollectionItem
-//
-//    var body: some View {
-//        ScrollView {
-//            switch item {
-//            case let .font(font):
-//                FontInfoPreview(font: font)
-//            case let .fontFamily(fontFamily):
-//                FontFamilyInfoPreview(family: fontFamily)
-//            }
-//        }
-//    }
-//}
-//
-//struct ItemTypeInfoPreview_Previews: PreviewProvider {
-//    static var previews: some View {
-//
-//        let attributes: [FontAttributeKey: String] =  {
-//            var ats = Dictionary(
-//                uniqueKeysWithValues: zip(
-//                    FontAttributeKey.allCases,
-//                    String.alphabet.accumulatingStrings()
-//                )
-//            )
-//            ats[.copyright] = String.alphabet + String.alphabet + String.alphabet + String.alphabet
-//            return ats
-//        }()
-//
-//        let fonts = (1...10).map { int in
-//            Font(
-//                url: URL(fileURLWithPath: NSTemporaryDirectory()),
-//                name: "Chicken \(int)",
-//                familyName: "Cheese",
-//                attributes: attributes
-//            )
-//        }
-//
-//        InfoPreview(
-//            item: FontCollectionItem.fontFamily(FontFamily(
-//                name: "Cheese",
-//                fonts: fonts)
-//            )
-//        )
-//    }
-//}
 
 fileprivate struct FontInfoPreview: View {
     var font: Font
